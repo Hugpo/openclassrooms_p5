d75ea0aa2532b6df4a02181eb52eb55b6ff66c4d
Hugo Dagnas <45885902+Hugpo@users.noreply.github.com>
2020-07-15 19:38:53 +0200

    Mise à jour du script
    Ajout d'une liste de sélection des groupes
    Ajout de try / catch
    Ajout de code erreur en lien avec la documentation

--- P5_dagnas_Script02.ps1
+++ P5_dagnas_Script02.ps1
@@ -1,5 +1,8 @@
 # Script 02 du projet 5 d'OpenClassRooms - Réalisé par Hugo DAGNAS
 # Script à visée éducative !
+# Version 1.1
+
+#Aide: https://github.com/Hugpo/openclassrooms_p5/wiki
 
 #Présentation du script à l'utilisateur
 Write-Host ''
@@ -7,15 +10,61 @@ Write-Host 'Script 02 - Hugo DAGNAS'
 Write-Host 'Bienvenue sur le script permettant de connaître les membres d''un groupe Active Directory.'
 Write-Host ''
 
-#Demande du paramètre (nom du groupe)
-$nameGroup = Read-Host 'Nom du groupe à lister'
+[array]$GroupsAD = Get-ADGroup -Filter "*"
+$GroupADNumber = $GroupsAD.count
 
-#Récupération des membres du groupe demandé
-$membersGroup = Get-ADGroupMember -Identity $nameGroup
+try
+{
+    Write-Host "Liste des groupes Active Directory disponible:"
+    for($i=0;$i -lt $GroupADNumber;$i++){
+        Write-Host $i ": " $GroupsAD[$i] 
+  }
+}
+catch
+{
+    Write-Host "Erreur: Impossible de récupérer la liste des groupes Active Directory disponible (Code erreur: 01)"
+    exit
+}
 
-#Affichage du résultat
-Write-Host 'Les membres du groupe' $nameGroup 'sont:'
-$membersGroup.name
+#Demande du groupe à rechercher
+try
+{
+    [int]$GroupSend = Read-Host "Indiquer le numéro du groupe à rechercher"
 
-#Fin du script - Version 1.0
-#Aide: https://github.com/Hugpo/openclassrooms_p5/wiki
+}
+catch
+{
+   Write-Host "Erreur: La donnée fournie n'est pas un nombre entier (Code erreur: 02)"
+   exit 
+}
+
+try
+{
+    $GroupSearch = $GroupsAD[$GroupSend]
+    $membersGroup = Get-ADGroupMember -Identity $GroupSearch
+}
+catch
+{
+  Write-Host "Erreur: Impossible de récupérer les membres du groupe demandé. (Code erreur: 03)"
+  exit   
+}
+
+try
+{
+  Write-Host 'Les membres du groupe' $GroupSearch 'sont:'
+  if ($membersGroup.count -eq 0)
+   {
+     Write-Host "Aucun membre"
+   }
+   else
+   {
+      $membersGroup.name 
+   }  
+}
+catch
+{
+  Write-Host "Erreur: Impossible d'afficher les membres du groupe demandé. (Code erreur: 04)"
+  exit    
+}
+
+#Fin de script
