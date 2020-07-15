23e30b5deeaf60562255c8a93a678a8f238f9a4f
Hugo Dagnas <45885902+Hugpo@users.noreply.github.com>
2020-07-15 20:10:51 +0200

    Update P5_dagnas_Script03.ps1
    Ajout d'une liste des utilisateurs
    Ajout des erreurs
    Version: 1.1

--- P5_dagnas_Script03.ps1
+++ P5_dagnas_Script03.ps1
@@ -1,21 +1,79 @@
 # Script 03 du projet 5 d'OpenClassRooms - Réalisé par Hugo DAGNAS
 # Script à visée éducative !
+# Version 1.1
 
-#Présentation du script à l'utilisateur
+# Aide: https://github.com/Hugpo/openclassrooms_p5/wiki
+
+# Présentation du script à l'utilisateur
 Write-Host ''
 Write-Host 'Script 03 - Hugo DAGNAS'
 Write-Host 'Bienvenue sur le script permettant de connaître les groupes d''un utilisateur Active Directory.'
 Write-Host ''
 
-#Demande du paramètre (nom de l'utilisateur)
-$nameUser = Read-Host 'Nom de l''utilisateur à lister'
+# Récupération de tous les membres de l'Active Directory
+[array]$MembersAD = Get-ADUser -Filter "*"
+$MembersADNumber = $MembersAD.count
 
-#Récupération des membres du groupe demandé
-$groupsMember = Get-ADUser $nameUser -Properties MemberOf
+# Affichage de tous les membres de l'Active Directory
+# Soit réussite de l'opération, soit erreur (code: 01)
+try
+{
+    Write-Host "Liste des membres Active Directory disponible:"
+    for($i=0;$i -lt $MembersADNumber;$i++){
+        Write-Host $i ": " $MembersAD[$i] 
+    }
+}
+catch
+{
+    Write-Host "Erreur: Impossible de récupérer la liste des membres Active Directory disponible (Code erreur: 01)"
+    exit
+}
 
-#Affichage du résultat
-Write-Host 'Les groupes de l''utilisateur' $nameUser 'sont:'
-$groupsMember.MemberOf
+# Demande du membre à rechercher
+# Soit réussite de l'opération, soit erreur (code: 02)
+try
+{
+    [int]$MemberSend = Read-Host "Indiquer le numéro du membre à rechercher"
 
-#Fin du script - Version 1.0
-#Aide: https://github.com/Hugpo/openclassrooms_p5/wiki
+}
+catch
+{
+    Write-Host "Erreur: La donnée fournie n'est pas un nombre entier (Code erreur: 02)"
+    exit 
+}
+
+# Récupération des groupes du membre demandé
+# Soit réussite de l'opération, soit erreur (code: 03)
+try
+{
+    $MemberSearch = $MembersAD[$MemberSend]
+    $groupsMember = Get-ADUser $MemberSearch -Properties MemberOf
+}
+catch
+{
+    Write-Host "Erreur: Impossible de récupérer les groupes du membre demandé. (Code erreur: 03)"
+    exit   
+}
+
+# Affichage des groupes de l'utilisateur demandé
+# Si le membre n'a aucun groupe, le script affiche "Aucun groupe"
+# Soit réussite de l'opération, soit erreur (code: 04)
+try
+{
+  Write-Host 'Les groupe de l''utilisateur' $MemberSearch 'sont:'
+  if ($groupsMember.count -eq 0)
+   {
+     Write-Host "Aucun groupe"
+   }
+   else
+   {
+      $groupsMember.MemberOf 
+   }  
+}
+catch
+{
+  Write-Host "Erreur: Impossible d'afficher les groupes du membre demandé. (Code erreur: 04)"
+  exit    
+}
+
+#Fin du script
