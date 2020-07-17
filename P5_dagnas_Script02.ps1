a3dc96e25297e7e251c96c6aa0bf34ee5091af0b
Hugo Dagnas <45885902+hugodagnas@users.noreply.github.com>
2020-07-17 10:12:11 +0200

    Update P5_dagnas_Script02.ps1
    Menu de sélection plus intuitif
    Ajout de plusieurs erreurs possibles
    Création d'un fichier .txt de résultat
    Mise en place de Param()

--- P5_dagnas_Script02.ps1
+++ P5_dagnas_Script02.ps1
@@ -1,32 +1,34 @@
 # Script 02 du projet 5 d'OpenClassRooms - Réalisé par Hugo DAGNAS
 # Script à visée éducative !
-# Version 1.1
+# Version 1.2
 
-#Aide: https://github.com/Hugpo/openclassrooms_p5/wiki
+# Aide: https://github.com/hugodagnas/openclassrooms_p5/wiki
+
+# Indiquer le chemin de sauvegarde du fichier de résultat du script
+Param($PathResultFolder = "C:\Users\Administrateur\Documents")
 
 #Présentation du script à l'utilisateur
-Write-Host ''
-Write-Host 'Script 02 - Hugo DAGNAS'
-Write-Host 'Bienvenue sur le script permettant de connaître les membres d''un groupe Active Directory.'
-Write-Host ''
+Write-Host ""
+Write-Host "Script 02 - Hugo DAGNAS"
+Write-Host "Bienvenue sur le script permettant de connaître les membres d'un groupe Active Directory."
+Write-Host ""
 
 [array]$GroupsAD = Get-ADGroup -Filter "*"
-$GroupADNumber = $GroupsAD.count
 
 try
 {
     Write-Host "Liste des groupes Active Directory disponible:"
-    for($i=0;$i -lt $GroupADNumber;$i++){
-        Write-Host $i ": " $GroupsAD[$i] 
-  }
+    for($i=0;$i -lt $GroupsAD.count;$i++){
+        Write-Host $i ": " $GroupsAD.Name[$i]
+    }
 }
 catch
 {
-    Write-Host "Erreur: Impossible de récupérer la liste des groupes Active Directory disponible (Code erreur: 01)"
+    Write-Host "Erreur: Impossible de récupérer la liste des groupes Active Directory disponible (Code erreur: 2.01)"
     exit
 }
 
-#Demande du groupe à rechercher
+# Demande du groupe à rechercher
 try
 {
     [int]$GroupSend = Read-Host "Indiquer le numéro du groupe à rechercher"
@@ -34,10 +36,11 @@ try
 }
 catch
 {
-   Write-Host "Erreur: La donnée fournie n'est pas un nombre entier (Code erreur: 02)"
-   exit 
+    Write-Host "Erreur: La donnée fournie n'est pas un nombre entier (Code erreur: 2.02)"
+    exit 
 }
 
+# Récupération des membres du groupe demandé
 try
 {
     $GroupSearch = $GroupsAD[$GroupSend]
@@ -45,26 +48,49 @@ try
 }
 catch
 {
-  Write-Host "Erreur: Impossible de récupérer les membres du groupe demandé. (Code erreur: 03)"
-  exit   
+    Write-Host "Erreur: Impossible de récupérer les membres du groupe demandé. (Code erreur: 2.03)"
+    exit   
 }
 
+# Affichage du résultat de la recherche
 try
 {
-  Write-Host 'Les membres du groupe' $GroupSearch 'sont:'
-  if ($membersGroup.count -eq 0)
-   {
-     Write-Host "Aucun membre"
-   }
-   else
-   {
-      $membersGroup.name 
-   }  
+    Write-Host "Les membres du groupe" $GroupSearch.Name "sont:"
+
+    # Si aucun membre dans la groupe, affichage de la réponse "Aucun membre"
+    if ($membersGroup.count -eq 0)
+    {
+        $members = "Aucun membre"  
+        Write-Host "Aucun membre"
+    }
+    else
+    {
+        $membersGroup.name 
+        $members = $membersGroup.name
+    }  
 }
 catch
 {
-  Write-Host "Erreur: Impossible d'afficher les membres du groupe demandé. (Code erreur: 04)"
-  exit    
+    Write-Host "Erreur: Impossible d'afficher les membres du groupe demandé. (Code erreur: 2.04)"
+    exit    
 }
 
-#Fin de script
+# Création du fichier de sortie du script
+# Le lieu de l'enregistrement du fichier est défini en paramètre du script
+
+try
+{
+    $groupFile = $GroupSearch.Name
+    Out-File -FilePath "$PathResultFolder\AD_membreDuGroupe_$groupFile.txt" -InputObject "Les membres du groupe $groupFile sont:"
+    Add-Content -Path "$PathResultFolder\AD_membreDuGroupe_$groupFile.txt" -Value $members
+}
+catch
+{
+    Write-Host "Erreur: La création du fichier txt de sortie a échoué (Code erreur: 2.05)"
+    exit 
+}
+
+# Confirmation de la création du fichier texte 
+Write-Host "Un fichier texte avec le résultat a été généré: $PathResultFolder\AD_membreDuGroupe_$groupFile.txt"
+
+# Fin de script
